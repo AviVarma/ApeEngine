c3045c28421bd7bf941069dcc4ee3ba497a50ed6
Connor <connorross19@sky.com>
2021-02-16 21:44:33 +0000

    comment

--- ApeEngine/src/Test.cpp
+++ ApeEngine/src/Test.cpp
@@ -4,6 +4,6 @@
 namespace ApeEngine {
 	void hello(){
 		std::cout << "Ooga Booga me ape." << std::endl;
-		std::cout << "yooooo" << std::endl;
+		std::cout << "Alex is shit at apex" << std::endl;
 	}
 }
